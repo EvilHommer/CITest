36278b01498ffb6e9ae07e58367f5e94826cb57e
Andrii <kovalchukworks@gmail.com>
2019-12-06 17:25:58 +0200

    temp changes

--- include/impl.h
+++ include/impl.h
@@ -1,3 +1,34 @@
 #pragma once
 
 
+class Complex
+{
+public:
+    constexpr Complex(const int real, const int imagine)
+        :mReal(real),
+          mImagine(imagine){}
+
+    constexpr int getReal(void)const{
+        return mReal;
+    }
+    constexpr int getImagine(void)const{
+        return mReal;
+    }
+
+    constexpr Complex operator+(const Complex& op)
+    {
+       constexpr int real = this->mReal + op.getReal();
+       constexpr int imagine = this->mImagine + op.getImagine();
+       constexpr Complex sum{real, imagine};
+       return sum;
+    }
+
+    Complex operator-(const Complex& op);
+
+private:
+    const int mReal;
+    const int mImagine;
+};
+
+bool operator==(const Complex& op1, const Complex& op2);
+
--- src/impl.cpp
+++ src/impl.cpp
@@ -1,1 +1,45 @@
 #include "impl.h"
+
+
+//constexpr Complex::Complex(const int real, const int imagine)
+//    :mReal(real),
+//      mImagine(imagine)
+//{
+
+//}
+
+//int Complex::getReal(void)const
+//{
+//    return mReal;
+//}
+
+//int Complex::getImagine(void)const
+//{
+//    return mImagine;
+//}
+
+//Complex Complex::operator+(const Complex& op)
+//{
+//    int real = this->mReal + op.getReal();
+//    int imagine = this->mImagine + op.getImagine();
+//    Complex sum{real, imagine};
+//    return sum;
+//}
+
+Complex Complex::operator-(const Complex& op)
+{
+    int real = this->mReal - op.getReal();
+    int imagine = this->mImagine - op.getImagine();
+    Complex sum{real, imagine};
+    return sum;
+}
+
+bool operator==(const Complex& op1, const Complex& op2)
+{
+    return ((op1.getReal() == op2.getReal()) && (op1.getImagine() == op2.getImagine()));
+}
+
+
+
+
+
